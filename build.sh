a34487ea44187e58af1c67f819a9bcb0cc2da476
sulincix <parduscix@yandex.ru>
2023-02-10 12:40:39 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ mkdir /output -p
 apt install git grub-pc-bin grub-efi squashfs-tools mtools xorriso -y
 git clone https://gitlab.com/turkman/devel/assets/mkiso mkiso
 cd mkiso
-bash mkiso
+bash mkiso.sh
 mv turkman.iso /output/
