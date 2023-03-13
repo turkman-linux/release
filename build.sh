a2427eca2af197c1e9dbde92136576427813a101
sulincix <parduscix@yandex.ru>
2023-03-13 08:08:09 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ apt install git grub-pc-bin grub-efi squashfs-tools mtools xorriso -y
 for variant in minimal x11 ; do
     git clone https://gitlab.com/turkman/devel/assets/mkiso $variant
     cd $variant
-    if [ -f ../$variant ] ; then
-        install ../$variant custom
+    if [ -f ../$variant.sh ] ; then
+        install ../$variant.sh custom
     fi
     bash mkiso.sh
     mv turkman.iso /output/turkman-$variant.iso
