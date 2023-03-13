7c8d2ff1ef13e86bf338511290127e099c0b9c5f
sulincix <parduscix@yandex.ru>
2023-03-13 08:30:22 +0000

    Update x11.sh

--- x11.sh
+++ x11.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #X11 install
 ymp repo --update --allow-oem --ignore-gpg
-ymp it xinit xorg-server xterm elogind freetype xauth xkbcomp xkeyboard-config xf86-input-libinput --no-emerge --allow-oem
+ymp it xinit xorg-server xterm elogind freetype xauth xkbcomp xkeyboard-config @x11.drivers --no-emerge --allow-oem
 rc-update add elogind
 rc-update add eudev
 rc-update add xinit
